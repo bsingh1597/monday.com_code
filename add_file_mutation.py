49e86327ed7ccbf78f5552a9d63de584afc858fb
Bipul Bishal Singh <bipul392ssnl@gmail.com>
2024-08-14 16:27:03 -0400

    working code

--- add_file_mutation.py
+++ add_file_mutation.py
@@ -5,20 +5,18 @@ import cred
 import Constants
 
 headers = {
-    "Authorization": cred.API_TOKEN,
-    "Content-Type": "application/json"
+    "Authorization": cred.API_TOKEN
 }
 
 # apiQuery = 'mutation add_file($file: File!, $item: ID!) {add_file_to_column (item_id: $item, column_id:"files__1" file: $file) { id } }'
 # apiMap = json.dumps({"file" : "variables.file", "item" : "variables.item_id"}).replace('"', '\\"')
 payload = {
-    'query': 'mutation add_file($file: File!) {add_file_to_column (file: $file,item_id: 7227992789, column_id:"files__1" file: $file) {id}}', 
-    'map': '{"text":"variables.file"}',
+    'query': 'mutation ($file: File!) {add_file_to_column (file: $file,item_id: 7227992789, column_id:"files__1") {id}}'
 }
 files=[
 ('variables[file]',('filename',open('test.txt','rb'),'contenttype'))
 ]
 
-response = requests.post(Constants.URL, headers=headers,  data=payload, files=files) 
+response = requests.post(Constants.URL_FILE, headers=headers,  data=payload, files=files) 
 print(response.status_code)
 print(response.reason)
